aeed918d450cfa97c99a3275913bc938456fed36
Dheneshkumars <dheneshkumar2312@gmail.com>
2023-10-05 11:09:14 +0530

    console.log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AppComponent from './Components/GroupComponent/AppComponent';
 import schema from './schema.json';
 function App() {
+    console.log("hhi");
     return (
         <div className="App">
             <AppComponent
